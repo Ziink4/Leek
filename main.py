599bf41044f385b5149880fe82632133bd33c3a0
Florian Brenot <Ziink4@users.noreply.github.com>
2022-04-09 17:27:31 +0200

    Formatting

--- main.py
+++ main.py
@@ -2,38 +2,45 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from multiprocessing import Pool
 
+CRITICAL_MULTIPLIER = 1.3
+
 
 def clamp(v, lower, upper):
     return min(max(v, lower), upper)
 
 
-def getStatForPoints(pt):
-    return 2.0 * clamp(pt, 0, 100) + clamp(pt - 100, 0, 200) + 0.5 * clamp(pt - 300, 0, 400) + (1.0 / 3.0) * clamp(
-        pt - 700, 0, 1200)
+def characteristic(points):
+    return 2.0 * clamp(points, 0, 100) \
+           + clamp(points - 100, 0, 200) \
+           + 0.5 * clamp(points - 300, 0, 400) \
+           + (1.0 / 3.0) * clamp(points - 700, 0, 1200)
 
 
-def bestRepartitionForPoints(maxPoints):
-    bestMult = [0, 0, 1]
-    for st in range(maxPoints + 1):
-        strStat = getStatForPoints(st)
-        agi = maxPoints - st
-        agiStat = getStatForPoints(agi)
-        critProb = agiStat / 1000.0
-        strMult = (1.0 + strStat / 100.0)
-        agiMult = critProb * 1.3 + (1.0 - critProb)
-        mult = strMult * agiMult
-        if mult > bestMult[2]:
-            bestMult = [st, agi, mult]
+def critical_chance(agility):
+    return agility / 1000.0
 
-    return bestMult
+
+def damage_multiplier(strength):
+    return 1.0 + strength / 100.0
+
+
+def optimal_strength_agility_split(max_points):
+    best_mult = [0, 0, 1]
+    for str_points in range(max_points + 1):
+        str_stat = characteristic(str_points)
+        agi_points = max_points - str_points
+        agi_stat = characteristic(agi_points)
+        crit_chance = critical_chance(agi_stat)
+        str_mult = damage_multiplier(str_stat)
+        agi_mult = crit_chance * CRITICAL_MULTIPLIER + (1.0 - crit_chance)
+        mult = str_mult * agi_mult
+        if mult > best_mult[2]:
+            best_mult = [str_points, agi_points, mult]
+
+    return best_mult
 
 
 if __name__ == '__main__':
-    print(getStatForPoints(100))
-    print(getStatForPoints(300))
-    print(getStatForPoints(700))
-    print(getStatForPoints(1300))
-
     x = []
     y = []
     ym = []
@@ -41,7 +48,7 @@ if __name__ == '__main__':
     zm = []
     m = []
     with Pool(10) as p:
-        r = list(tqdm(p.imap(bestRepartitionForPoints, range(1, 1001))))
+        r = list(tqdm(p.imap(optimal_strength_agility_split, range(1, 1001))))
 
     print(r)
 
@@ -65,7 +72,7 @@ if __name__ == '__main__':
     ax.legend()
     plt.show()
 
-    ret = bestRepartitionForPoints(1780)
+    ret = optimal_strength_agility_split(1780)
     print(ret)
-    print(getStatForPoints(ret[0]))
-    print(getStatForPoints(ret[1]))
+    print(characteristic(ret[0]))
+    print(characteristic(ret[1]))
